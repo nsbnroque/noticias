a6dd9e0131f2df7a22d02ced8671d5eb7536da46
Natalia Roque <nsbnroque@gmail.com>
2023-04-09 07:02:35 -0300

    Estrutura inicial do projeto

---
+++ api-noticias/src/main/java/com/noticias/apinoticias/ApiNoticiasApplication.java
@@ -0,0 +1,13 @@
+package com.noticias.apinoticias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNoticiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNoticiasApplication.class, args);
+	}
+
+}
---
+++ api-noticias/src/test/java/com/noticias/apinoticias/ApiNoticiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noticias.apinoticias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNoticiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
